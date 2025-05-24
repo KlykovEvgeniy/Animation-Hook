b39efd5cb2d3d5068f706b0d910e5cc122290bb4
KlykovEvgeniy <klikovevgeniy2000@mail.ru>
2025-05-24 19:06:43 +0300

    Animation

---
+++ src/Animation.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from "react";
+import './animation.css';
+
+export const Animation = ({ children, startedVis }) => {
+    const child = useRef(null)
+    useEffect(() => {
+        if (startedVis) {
+            child.current.classList.add('vis');
+            child.current.classList.remove('none-vis')
+        } else {
+            child.current.classList.add('none-vis')
+        }
+    }, []);
+
+    useEffect(() => {
+        document.addEventListener('scroll', handleScroll)
+
+        return () => {
+            document.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+    function handleScroll(event) {
+        const target = event.target;
+        const docElem = target.documentElement;
+
+        if (docElem.scrollHeight - (docElem.scrollTop + window.innerWidth) < 100) {
+            child.current.classList.remove('none-vis')
+            child.current.classList.add('vis');
+        } else {
+            child.current.classList.add('none-vis')
+        }
+    }
+
+    return (
+        <>
+            <div ref={child}>{children}</div>
+        </>
+    )
+}
---
+++ src/Animation.tsx
@@ -0,0 +1,44 @@
+import { FC, ReactNode, RefObject, useEffect, useRef } from "react";
+import './animation.css';
+
+interface FadeProps {
+    children: ReactNode,
+    startedVis: boolean,
+}
+export const Animation: FC<FadeProps> = ({ children, startedVis }) => {
+    const child = useRef<HTMLElement | null>(null)
+    useEffect(() => {
+        if (startedVis) {
+            child.current?.classList.add('vis');
+            child.current?.classList.remove('none-vis')
+        } else {
+            child.current?.classList.add('none-vis')
+        }
+    }, []);
+
+    useEffect(() => {
+        document.addEventListener('scroll', handleScroll)
+
+        return () => {
+            document.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
+    function handleScroll(event: Event) {
+        const target = event.target as Document;
+        const docElem = target.documentElement;
+
+        if (docElem.scrollHeight - (docElem.scrollTop + window.innerWidth) < 100) {
+            child.current?.classList.remove('none-vis')
+            child.current?.classList.add('vis');
+        } else {
+            child.current?.classList.add('none-vis')
+        }
+    }
+
+    return (
+        <>
+            <div ref={child as RefObject<HTMLDivElement>}>{children}</div>
+        </>
+    )
+}
